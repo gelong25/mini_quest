307d940077c51b8f0e2f8d0f3936d005a651fcd4
groom <groom@groomui-MacBookPro.local>
2024-11-02 21:05:44 +0900

    객체 미니퀘스트 1번

---
+++ src/week02/object/miniQuest01.js
@@ -0,0 +1,15 @@
+/*
+다음 요구 사항을 충족하는 객체 myPet 만들기 
+
+1. myPet 객체는 name (이름)과 type (종류), 두 개의 속성을 가져야 함 
+2. name 값 : "Momo"
+3. type 값 : "Cat"
+4. 객체를 생성한 후 console.log를 사용해서 myPet 객체의 name과 type을 출력하기 
+*/
+
+const myPet = {
+    name: "Momo",
+    type: "Cat"
+};
+
+console.log(myPet.name, myPet.type);
